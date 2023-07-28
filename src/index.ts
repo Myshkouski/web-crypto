11c5df584be31fcbe449675126416a79d108cbbf
Alexei Myshkouski <alexeimyshkouskI@gmail.com>
2023-07-28 18:59:27 +0300

    add bytesToString to export

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ export * from "./aes.js";
 export * from "./digest.js";
 export {
   bytesToHex,
+  bytesToString,
   hexToBytes,
   randomBytes,
-  stringToBytes,
+  stringToBytes
 } from "./helpers.js";
 export { randomUUID } from "uncrypto";
 // export * from './derive'
